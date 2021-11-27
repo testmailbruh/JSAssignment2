678dd440df92dd68b6765a7d720fdf2b7b5a4bc2
testmailbruh <81257163+testmailbruh@users.noreply.github.com>
2021-11-27 18:37:00 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,21 @@
+var person1 = {
+    FirstName:'Shobhit',
+    LastName:'Kaothalkar',
+    OfficeAddress:'Sagar, Madhya Pradesh'
+}
+
+var person2 = {
+    FirstName:'Divyansh',
+    LastName:'Soni',
+    OfficeAddress:'Gwalior, Madhya Pradesh'
+}
+
+var person3 = {
+    FirstName:'Nischal',
+    LastName:'Singh',
+    OfficeAddress:'Jabalpur, Madhya Pradesh'
+}
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
